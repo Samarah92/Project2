353ed097e003ff4ad3b2b55103f112bec0eb7d70
Glitch (illustrious-spectacled-meerkat) <none>
2021-09-03 13:47:55 +0000

    🐕🕋 Updated with Glitch

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,39 +1,41 @@
 "use strict";
+
 const uuid = require('uuid');
 
 const logger = require("../utils/logger");
-const playlistStore = require('../models/playlist-store');
-const accounts = require ('./accounts.js');
+const stationStore = require('../models/station-store');
+const analytics = require('../utils/analytics');
+const station = require('../controllers/station')
 
 const dashboard = {
   index(request, response) {
-    logger.info('dashboard rendering');
-    const loggedInUser = accounts.getCurrentUser(request);
-    const viewData = {
-      title: 'Playlist Dashboard',
-      playlists: playlistStore.getUserPlaylists(loggedInUser.id),
-    };
-    logger.info('about to render', playlistStore.getAllPlaylists());
-    response.render('dashboard', viewData);
-  },
+const dashboard = analytics.getUpdateWeather(station);
+    for (station=this.stations) {
+      analytics.updateWeather(station);
+    }
+const viewData = {
+      title: 'Station',
+      station: stationStore.getAllStations(),
   
-   deletePlaylist(request, response) {
-    const playlistId = request.params.id;
-    logger.debug(`Deleting Playlist ${playlistId}`);
-    playlistStore.removePlaylist(playlistId);
+    };
+    logger.info('about to render', stationStore.getAllStations());
+    response.render('dashboard', viewData);
+},
+  
+   deleteStation(request, response) {
+    const stationId = request.params.id;
+    logger.debug(`Deleting Station ${stationId}`);
+    stationStore.removeStation(stationId);
     response.redirect('/dashboard');
   },
   
-  addPlaylist(request, response) {
-    const loggedInUser = accounts.getCurrentUser(request);
-    const newPlayList = {
+    addStation(request, response) {
+    const newStation = {
       id: uuid.v1(),
-      userid: loggedInUser.id,
       title: request.body.title,
-      songs: [],
+      readings: [],
     };
-    logger.debug('Creating a new Playlist', newPlayList);
-    playlistStore.addPlaylist(newPlayList);
+    stationStore.addStation(newStation);
     response.redirect('/dashboard');
   },
 };
---
+++ controllers/station.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const uuid = require('uuid');
+
+const logger = require('../utils/logger');
+const stationStore = require('../models/station-store');
+
+const station = {
+  index(request, response) {
+    const stationId = request.params.id;
+    logger.debug('Station id = ', stationId);
+    const viewData = {
+      title: 'Station',
+      station: stationStore.getStation(stationId),
+    };
+    response.render('station', viewData);
+  },
+  
+  deleteReading(request, response) {
+    const stationId = request.params.id;
+    const readingId = request.params.readingid;
+    logger.debug(`Deleting Reading ${readingId} from Station ${stationId}`);
+    stationStore.removeReading(stationId, readingId);
+    response.redirect('/station/' + stationId);
+  },
+  
+  addReading(request, response) {
+    const stationId = request.params.id;
+    const station = stationStore.getStation(stationId);
+    const newReading = {
+      id: uuid.v1(),
+      code: Number(request.body.code),
+      temperature: Number(request.body.temperature),
+      windSpeed: Number(request.body.windSpeed),
+    };
+    logger.debug('New Reading = ', newReading);
+    stationStore.addReading(stationId, newReading);
+    response.redirect('/station/' + stationId);
+  },
+};
+
+module.exports = station;
